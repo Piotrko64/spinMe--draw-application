0e09792934dcfc19a481b0eb58a5d7f6e91f876e
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-05 19:39:38 +0200

    manual test working SliceCircle

--- src/components/circleWheelComponents/wheelerCircleComponents/SliceCircle.tsx
+++ src/components/circleWheelComponents/wheelerCircleComponents/SliceCircle.tsx
@@ -1,3 +1,9 @@
-export function SliceCircle() {
-    return <></>;
+import classes from "./sliceCircle.module.css";
+export function SliceCircle({ color, angle, index }: { color: string; angle: number; index: number }) {
+    const stylesSlice = {
+        background: `conic-gradient(${color} 0deg, ${color} ${angle}deg, transparent ${angle}deg, transparent 360deg)`,
+        transform: `rotate(${index * angle}deg)`,
+    };
+
+    return <div className={classes.slice} style={stylesSlice}></div>;
 }
--- src/components/circleWheelComponents/wheelerCircleComponents/TheCircle.tsx
+++ src/components/circleWheelComponents/wheelerCircleComponents/TheCircle.tsx
@@ -1,5 +1,10 @@
+import { SliceCircle } from "./SliceCircle";
 import classes from "./theCircle.module.css";
 
 export function TheCircle() {
-    return <div className={classes.circle}></div>;
+    return (
+        <div className={classes.circle}>
+            <SliceCircle color="blue" angle={75} index={0} />
+        </div>
+    );
 }
