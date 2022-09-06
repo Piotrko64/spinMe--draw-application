15b783c6ef22bdc07bfc0df85e943b92c28ce30b
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-06 22:17:55 +0200

    preparation to handle modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { ActionButtons } from "./components/buttonsUnderCircle/ActionsButton";
 import { CanvasContainer } from "./components/canvas/CanvasContainer";
 import { ChoicesList } from "./components/choicesList/ChoicesList";
 import { CircleWheelEntireComponent } from "./components/circleWheelComponents/CircleWheelEntireComponent";
+import { useCircleSettings } from "./store/useCircleSettings";
+import { ModalWinner } from "./ui/ModalWinner";
 
 function App() {
+    const openModalWinner = useCircleSettings((state) => state.activeModal);
+
     return (
         <div className={classes.app}>
             <CanvasContainer>
                 <CircleWheelEntireComponent />
                 <ActionButtons />
                 <ChoicesList />
+                {openModalWinner && <ModalWinner />}
             </CanvasContainer>
         </div>
     );
