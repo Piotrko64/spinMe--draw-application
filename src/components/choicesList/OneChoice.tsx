8608ca8854f845be1abfce4f11215b21a6402346
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-06 20:12:15 +0200

    add delete button

--- src/components/choicesList/OneChoice.tsx
+++ src/components/choicesList/OneChoice.tsx
@@ -1,8 +1,17 @@
+import { useCircleSettings } from "../../store/useCircleSettings";
 import { ColorsInput } from "./inputsChoices/ColorsInput";
 import { TextInputChoice } from "./inputsChoices/TextInputChoice";
 import classes from "./oneChoice.module.css";
 
 export function OneChoice({ title, id, color }: { title: string; id: string; color: string }) {
+    const slices = useCircleSettings((state) => state.slices);
+    const updateStateSlices = useCircleSettings((state) => state.updateAllSlices);
+
+    function deleteSlice() {
+        const cloneSlices = [...slices];
+        updateStateSlices(cloneSlices.filter((slice) => slice.id !== id));
+    }
+
     return (
         <div className={classes.oneChoice}>
             <div className={classes.dragDots}>
@@ -13,6 +22,11 @@ export function OneChoice({ title, id, color }: { title: string; id: string; col
 
             <TextInputChoice id={id} title={title} />
             <ColorsInput color={color} id={id} />
+            {slices.length > 2 && (
+                <button className={classes.buttonDelete} onClick={deleteSlice}>
+                    X
+                </button>
+            )}
         </div>
     );
 }
