e4ba9a94db9076f912030938ac614af285d09a4c
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-07 10:50:00 +0200

    fix display modal

--- src/ui/BaseModal.tsx
+++ src/ui/BaseModal.tsx
@@ -5,7 +5,21 @@ import classes from "./baseModal.module.css";
 export function BaseModal({ children }: ChildrenProps) {
     return (
         <>
-            <div className={classes.modalBackground}></div>
+            <motion.div
+                layout
+                animate={{ opacity: 1 }}
+                exit={{
+                    y: -50,
+                    opacity: 0,
+                    scale: 2,
+                }}
+                transition={{
+                    duration: 0.3,
+                }}
+                initial={{ opacity: 0 }}
+            >
+                <div className={classes.modalBackground}></div>
+            </motion.div>
             <div className={classes.position}>
                 <motion.div
                     layout
@@ -16,12 +30,12 @@ export function BaseModal({ children }: ChildrenProps) {
                         scale: 2,
                     }}
                     transition={{
-                        duration: 0.35,
+                        duration: 0.22,
                         type: "spring",
                         damping: 3,
-                        mass: 0.75,
+                        mass: 0.65,
                         stiffness: 50,
-                        delay: 0.2,
+                        delay: 0,
                     }}
                     initial={{ scale: 0 }}
                 >
