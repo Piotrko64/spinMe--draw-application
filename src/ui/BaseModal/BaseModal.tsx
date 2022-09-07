b43fb6d74f5e2d6a5089965ac107609c25986014
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-07 18:27:34 +0200

    delete exit animation from modal

--- src/ui/BaseModal/BaseModal.tsx
+++ src/ui/BaseModal/BaseModal.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { ChildrenProps } from "../../@types/ChildrenProps";
 import classes from "./baseModal.module.css";
 
@@ -8,9 +8,6 @@ export function BaseModal({ children }: ChildrenProps) {
             <motion.div
                 layout
                 animate={{ opacity: 1 }}
-                exit={{
-                    opacity: 0,
-                }}
                 transition={{
                     duration: 0.2,
                 }}
@@ -20,29 +17,20 @@ export function BaseModal({ children }: ChildrenProps) {
             </motion.div>
 
             <div className={classes.position}>
-                <AnimatePresence exitBeforeEnter>
-                    <motion.div
-                        animate={{ scale: 1 }}
-                        exit={{
-                            scale: 0,
-                            transition: {
-                                delay: 1,
-                                duration: 5,
-                            },
-                        }}
-                        transition={{
-                            duration: 0.12,
-                            type: "spring",
-                            damping: 4,
-                            mass: 0.35,
-                            stiffness: 50,
-                            delay: 0,
-                        }}
-                        initial={{ scale: 0 }}
-                    >
-                        <div className={classes.modal}>{children}</div>
-                    </motion.div>
-                </AnimatePresence>
+                <motion.div
+                    animate={{ scale: 1 }}
+                    transition={{
+                        duration: 0.12,
+                        type: "spring",
+                        damping: 4,
+                        mass: 0.35,
+                        stiffness: 50,
+                        delay: 0,
+                    }}
+                    initial={{ scale: 0 }}
+                >
+                    <div className={classes.modal}>{children}</div>
+                </motion.div>
             </div>
         </>
     );
